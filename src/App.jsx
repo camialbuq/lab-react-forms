19df8ba2e55bdbf71c265a507f425d002e291e05
camilaalbuq <cam.albuquer@gmail.com>
2024-04-23 22:40:50 +0200

    lab solved 23-04-24 no bonus

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,124 @@ import studentsData from "./assets/students.json";
 function App() {
   const [students, setStudents] = useState(studentsData);
 
+  //adding variables to the form -Iteration 01
+  //create a state variable for each input.
+  const [fullName, setFullName] = useState("");
+  const [image, setImage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [program, setProgram] = useState("");
+  const [graduationYear, setGraduationYear] = useState(2023);
+  const [graduated, setGraduated] = useState(false);
+
+  //handling the state - Iteration 02
+  const handleFullNameInput = (e) => setFullName(e.target.value);
+  const handleImageInput = (e) => setImage(e.target.value);
+  const handlePhoneInput = (e) => setPhone(e.target.value);
+  const handleEmailInput = (e) => setEmail(e.target.value);
+  const handleProgramInput = (e) => setProgram(e.target.value);
+  const handleGraduationYearInput = (e) => setGraduationYear(e.target.value);
+  const handleGraduatedInput = (e) => setGraduated(e.target.checked);
+  // e.target.checked is a boolean value from the `checkbox` input
+  //the form will be inside of the return function and we will have the value property within {}
+  //to be able to use the variables created in state above
+
+  //need a function to add this new student and set the new state
+  const handleAddStudent = (student) => {
+    setStudents((prevStudents) => [student, ...prevStudents]);
+  };
+
+  //to handle the onSubmit behaviour and prevent the whole page from loading again
+  //since from form onSubmit will reload and lose all the state saved , then we do this function
+  const handleSubmit = (e) => {
+    e.preventDefault(); //prevent browser default behavious
+    //access the input data stored in the state (state variables title, director, etc.).
+    const newStudent = {
+      fullName,
+      image,
+      phone,
+      email,
+      program,
+      graduationYear,
+      graduated,
+    };
+    console.log("Submitted: ", newStudent);
+    //at the moment only adding to the console, but it needs to be added to the MovieList component
+    //Whenever the form in the AddMovie component is submitted we can call addNewMovie and pass it to the newMovie object.
+
+    // Add new student to students array
+    handleAddStudent(newStudent);
+
+    // Reset the state to empty the form again
+    setFullName("");
+    setImage("");
+    setPhone("");
+    setProgram("");
+    setGraduationYear(2023);
+    setGraduated(false);
+  };
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={handleFullNameInput}
+            />
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              value={image}
+              onChange={handleImageInput}
+            />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              value={phone}
+              onChange={handlePhoneInput}
+            />
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailInput}
+            />
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select
+              name="program"
+              value={program}
+              onChange={handleProgramInput}
+            >
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -55,6 +139,8 @@ function App() {
             <input
               name="graduationYear"
               type="number"
+              value={graduationYear}
+              onChange={handleGraduationYearInput}
               placeholder="Graduation Year"
               minLength={4}
               maxLength={4}
@@ -65,19 +151,21 @@ function App() {
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input
+              name="graduated"
+              type="checkbox"
+              checked={graduated}
+              onChange={handleGraduatedInput}
+            />
           </label>
 
           <button type="submit">Add Student</button>
         </div>
-
       </form>
       {/* FORM END */}
 
-
       {/* TABLE/LIST HEADER */}
       <TableHeader />
-
 
       {/* STUDENT LIST */}
       {students &&
---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddStudent() {
+  return <div>AddStudent</div>;
+}
+
+export default AddStudent;
